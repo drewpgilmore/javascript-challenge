5ef8db042765c8bb6b3a8c15aedd33fdcfe9d42b
drewpgilmore@gmail.com <drewpgilmore@gmail.com>
2020-08-26 23:21:03 -0700

    added date search function

--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,30 @@
 // from data.js
 var tableData = data;
 
+//store date selected by 
+//var date_selected = document.getElementById("datetime");
+function search_data(date) {
+    // empty list for matching sightings to be appended to table
+    matches = [];
+    // loop through data to search for matching dates
+    tableData.forEach(function(sighting){
+        if (sighting.datetime == date) {
+            matches.push(sighting);
+        }
+    });
+    return matches;
+}
+
+
+
 // YOUR CODE HERE!
+
+// function for "filter table" button 
+function handleClick() {
+    // insert table appending functionality here
+    // placeholder alert
+    var date = document.getElementById("datetime");
+    matchedSightings = search_data(date);
+    numMatches = matchedSightings.length;
+    alert(numMatches);
+}
